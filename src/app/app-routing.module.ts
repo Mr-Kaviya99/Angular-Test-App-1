b95f10464b7c600122a0592f7379cbb60edd4802
Mr-Kaviya99 <kavinduhasintha9@gmail.com>
2022-04-28 09:27:10 +0530

    init

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,28 @@
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {NotFoundPageComponent} from "./modules/share/components/not-found-page/not-found-page.component";
+import {LoginComponent} from "./components/share/login/login.component";
+import {SignUpComponent} from "./components/share/sign-up/sign-up.component";
+
+const routes: Routes = [
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'login', component: LoginComponent},
+  {path: 'sign-up', component: SignUpComponent},
+  {
+    path: 'share', loadChildren: () => import('./modules/share/share.module').then(m => m.ShareModule)
+  },
+  {
+    path: 'home', loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule)
+  },
+  {
+    path: 'dashboard', loadChildren: () => import('./modules/dashboard/dashboard.module').then(m => m.DashboardModule)
+  },
+  {path: '**', component: NotFoundPageComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {
+}
